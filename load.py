448a60b0c386991e154827d4bfb525ea0c8e6d99
mayank31398 <mayank31398@gmail.com>
2023-03-12 20:59:39 +0530

    :ambulance: test exported model

--- load.py
+++ load.py
@@ -6,8 +6,6 @@ from optimum.onnxruntime import ORTModelForCausalLM
 def get_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("--model_name", required=True, help="model name")
-    parser.add_argument("--save_path", required=True, help="save directory")
-    parser.add_argument("--merge_graphs", action="store_true", help="merge the 2 graphs")
     args = parser.parse_args()
     return args
 
@@ -16,12 +14,14 @@ def main() -> None:
     args = get_args()
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-    tokenizer.save_pretrained(args.save_path)
+    tokenizer.add_special_tokens({"pad_token": "<|endoftext|>"})
+    model = ORTModelForCausalLM.from_pretrained(args.model_name, trust_remote_code=True)
 
-    model = ORTModelForCausalLM.from_pretrained(
-        args.model_name, export=True, trust_remote_code=True, use_merged=args.merge_graphs
-    )
-    model.save_pretrained(args.save_path)
+    inputs = tokenizer(["Hello, how are you", "Ayo!"], return_tensors="pt", padding=True)
+    outputs = model.generate(**inputs)
+    outputs = tokenizer.batch_decode(outputs)
+
+    print(outputs)
 
 
 if __name__ == "__main__":
